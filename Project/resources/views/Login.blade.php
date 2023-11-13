bdda2f583fe69afd64f8670ae56e6b83003d081a
Michael Setiabudi <michael.s22@mhs.istts.ac.id>
2023-11-13 15:32:19 +0700

    Add Website Logo & Show Password

--- Project/resources/views/Login.blade.php
+++ Project/resources/views/Login.blade.php
@@ -5,12 +5,23 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Login</title>
+    <link rel="icon" href="../Assets/logoistts.png" type="image/x-icon">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
         <style>
           #RememberMe:hover {
               cursor: pointer;
           }
+          #loginbtn:hover{
+            background-color: #007bff;
+          }
+          .show-password {
+            position: absolute;
+            right: 10px;
+            top: 50%;
+            transform: translateY(-50%);
+            cursor: pointer;
+        }
       </style>
 </head>
 
@@ -18,7 +29,7 @@
     <header>
         <nav class="navbar">
             <div class="container align-items-center">
-                <img class="pl-5 pt-2" style="height: 7em; width: 7em;" src="../Assets/LogoISTTS.png" alt=""
+                <img class="pl-5 pt-2" style="height: 7em; width: 7em;" src="../Assets/logoistts.png" alt=""
                     srcset="">
             </div>
         </nav>
@@ -40,13 +51,16 @@
                         </div>
                         <div class="form-group pt-2">
                           <p class="p-0"><b>Password</b></p>
-                          <input type="password" name="Password" id=""placeholder="Password"style="width:100%;height:3em;">
+                          <div class="password-container">
+                            <input type="password" id="password" name="password" style="width:100%;height:3em;"placeholder="Password" required>
+                            <span class="show-password" onclick="togglePassword()">👁️</span>
+                        </div>
                         </div>
                         <div class="form-group pt-2">
                           <input type="checkbox" name="RememberMe" id="RememberMe"><span class="p-2">Remember Me</span>
                         </div>
                         <div class="form-group pt-2">
-                          <button type="submit" name="LoginBtn" class="btn btn-info btn-rounded text-light pt-2 pb-2 pl-4 pr-4">Sign In</button>
+                          <button type="submit"id="loginbtn" name="LoginBtn" class="btn btn-info btn-rounded text-light pt-2 pb-2 pl-4 pr-4">Sign In</button>
                         </div>
                       </form>
                     </div>
@@ -57,6 +71,19 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous">
     </script>
-</body>
+    <script>
+      function togglePassword() {
+          var passwordField = document.getElementById("password");
+          var showPasswordIcon = document.querySelector(".show-password");
 
+          if (passwordField.type === "password") {
+              passwordField.type = "text";
+              showPasswordIcon.textContent = "👁️";
+          } else {
+              passwordField.type = "password";
+              showPasswordIcon.textContent = "👁️";
+          }
+      }
+  </script>
+</body>
 </html>
