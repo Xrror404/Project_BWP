a31d4b366b686082a03e62a6579f2d155cf22ef2
Michael Setiabudi <michael.s22@mhs.istts.ac.id>
2023-11-14 15:12:54 +0700

    Add Anti SQLI and Change Login Method

--- Project/app/Http/Controllers/LoginController.php
+++ Project/app/Http/Controllers/LoginController.php
@@ -4,29 +4,54 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 
 class LoginController extends Controller
 {
-    public function ToLogin(Request $request){
+    public function ToLogin(Request $request)
+    {
         return view('Login');
     }
-    public function AreUserExist(Request $request){
+    public function AreUserExist(Request $request)
+    {
+        // Validate the user input
+        $validatedData = $request->validate([
+            'Username' => 'required|string|max:255',
+            'password' => 'required|string|max:255',
+        ]);
 
-        $daftarMhs=DB::connection("KoneksiDatabase")->table("mahasiswa")->get();
+        // Check for SQL injection in the input
+        $inputContainsSQL = $this->containsSQL($validatedData['Username']) || $this->containsSQL($validatedData['password']);
 
-        $Username = $request->input('Username');
-        $Password = $request->input('password');
-        $isValid=false;
+        if ($inputContainsSQL) {
+            return redirect('Login')->with([
+                "pesansukses" => "Input contains SQL keywords. Login failed.",
+                "tipe" => "gagal"
+            ]);
+        }
+
+        $Username = $validatedData['Username'];
+        $Password = $validatedData['password'];
+        $isValid = false;
+
+        $daftarMhs = DB::connection("KoneksiDatabase")
+            ->table("mahasiswa")
+            ->where('nama_mhs', $Username)
+            ->where('password_mhs', $Password)
+            ->get();
+
+        // Check if the provided credentials are valid (without hashing the password)
         foreach ($daftarMhs as $Mahasiswa) {
-            if ($Mahasiswa->nama_mhs === $Username) {
-                if ($Mahasiswa->nama_mhs === $Username) {
-                    $isValid = true;
-                }
+            if ($Mahasiswa->nama_mhs === $Username && $Mahasiswa->password_mhs === $Password) {
+                $isValid = true;
+                break;
             }
         }
+
+        // Redirect based on validation and SQL injection check result
         if ($isValid) {
             return redirect('Login')->with([
-                "pesansukses" => "Berhasil Login $Username dengan password $Password",
+                "pesansukses" => "Berhasil Login $Username",
                 "tipe" => "sukses"
             ]);
         } else {
@@ -35,5 +60,17 @@ class LoginController extends Controller
                 "tipe" => "gagal"
             ]);
         }
+    }
+
+    private function containsSQL($input)
+    {
+        $sqlKeywords = ['SELECT', 'INSERT', 'UPDATE', 'DELETE', 'FROM', 'WHERE', 'DROP', 'UNION', 'JOIN', 'AND', 'OR'];
+        foreach ($sqlKeywords as $keyword) {
+            if (str_contains(strtoupper($input), $keyword)) {
+                return true;
+            }
+        }
+
+        return false;
     }
 }
