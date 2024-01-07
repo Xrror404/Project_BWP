bc27812079a8a7f9ff5397fa9da886a68f7a65cf
Michael Setiabudi <michael.s22@mhs.istts.ac.id>
2024-01-07 14:51:36 +0700

    Update point.blade.php
    Add grouping antar point

--- Project/resources/views/point.blade.php
+++ Project/resources/views/point.blade.php
@@ -1,45 +1,71 @@
 @extends('layouts.SIM')
 
 @section('content')
-    <div class="card">
-        <div class="card-body">
-            <!-- Your existing content -->
-            <div class="container">
-                <h2 class="text-light">Point {{ Auth::user()->nama_user }}</h2>
-                <table class="table">
-                    <thead>
-                        <tr>
-                            <th scope="col">Tanggal</th>
-                            <th scope="col">Kegiatan</th>
-                            <th scope="col">Poin</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        @foreach ($poin_user as $poin)
-                            <tr>
-                                <td>{{ $poin->poins->poin_added_date ?? 'N/A' }}</td>
-                                <td> {{ $poin->poins->nama_acara ?? 'N/A' }}</td>
-                                <td>{{ $poin->poins->jml_poin ?? 'N/A' }}</td>
-                            </tr>
-                        @endforeach
-                        {{-- ini Yang Seharusnya V --}}
-                        {{-- @foreach ($poinAll->Orang as $poin)
-                            <tr>
-                                <td>{{ $poin->poin_added_date ?? 'N/A' }}</td>
-                                <td>{{ $poin->nama_acara ?? 'N/A' }}</td>
-                                <td>{{ $poin->jml_poin ?? 'N/A' }}</td>
-                            </tr>
-                        @endforeach --}}
-                    </tbody>
-                </table>
-
+    <div class="container m-2 d-flex justify-content-center align-items-center"style="background-color: #360000;width:auto;">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-6">
+                    <h1 class="text-light">Poin Kemahasiswaan</h1>
+                </div>
+                <div class="col-md-6">
+                    <button class="btn btn-secondary" onclick="printPage()">Cetak</button>
+                </div>
+            </div>
+            <div class="row">
+                @php
+                    $currentType = '';
+                @endphp
+                @foreach ($poin_user as $poin)
+                    @if ($poin->poins->tipe_acara != $currentType)
+                        @php
+                            $currentType = $poin->poins->tipe_acara;
+                        @endphp
+                        @if ($currentType == 0)
+                            <h2>Minat Bakat</h2>
+                        @elseif ($currentType == 1)
+                            <h2>Penalaran</h2>
+                        @elseif ($currentType == 2)
+                            <h2>Organisasi</h3>
+                            @elseif ($currentType == 3)
+                                <h2>Kerohanian</h2>
+                        @endif
+                        <table class="table">
+                            <thead>
+                                <tr>
+                                    <th scope="col"><b>Tanggal</b></th>
+                                    <th scope="col"><b>Kegiatan</b></th>
+                                    <th scope="col"><b>Poin</b></th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                    @endif
+                    <tr>
+                        <td>{{ $poin->poins->poin_added_date ?? 'N/A' }}</td>
+                        <td> {{ $poin->poins->nama_acara ?? 'N/A' }}</td>
+                        <td>{{ $poin->poins->jml_poin ?? 'N/A' }}</td>
+                    </tr>
+                    @if ($loop->last || $poin_user[$loop->index + 1]->poins->tipe_acara != $currentType)
+                        </tbody>
+                        </table>
+                    @endif
+                @endforeach
             </div>
         </div>
     </div>
-    <br>
     <script>
         function printPage() {
-            window.print();
+            var printContents = '';
+            var tables = document.getElementsByClassName('table');
+            for (var i = 0; i < tables.length; i++) {
+                printContents += tables[i].outerHTML;
+            }
+
+            var printWindow = window.open('', '_blank');
+            printWindow.document.write('<html><head><title>Print</title></head><body>');
+            printWindow.document.write(printContents);
+            printWindow.document.write('</body></html>');
+            printWindow.document.close();
+            printWindow.print();
         }
     </script>
 @endsection
