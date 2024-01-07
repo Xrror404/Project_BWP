acd1b9d3188bd633bac3b6377ba87888cba42b67
Michael Setiabudi <michael.s22@mhs.istts.ac.id>
2024-01-07 11:42:47 +0700

    Update pengaturan.blade.php

--- Project/resources/views/pengaturan.blade.php
+++ Project/resources/views/pengaturan.blade.php
@@ -2,57 +2,51 @@
 
 
 @section('content')
-
     <div class="container p-2"style="background-color: #360000;">
-            <div class="row p-1">
-                <div class="col-md-6 p-1">
-                    <h1 class="text-light">Pengaturan</h1>
-                </div>
-                <div class="col-md-6 p-1">
-                    <h6 class="card-title">
-                        <button onclick="toggleMode()">Toggle Mode</button>
-                    </h6>
-                </div>
-                <div style="float: left; width: 50%;color: white;">
-                    <h1>Notifikasi</h1>
-                    <br>
-                    <br>
-                    <h2 style="color: white">Pengumuman baru</h2>
-                    <br>
-                    <br>
-                    <h3 style="color: white">Pengingat pembayaran</h3>
-                </div>
-                <div style="float: left; width: 30%;color: white;">
-                    <h1>Push Notification</h1>
-                    <button id="aktivasiButton" style="background-color: red" onclick="showNotification()">Aktivasi</button>
-                    <br> <!-- Line break untuk memberikan jarak antara tombol dan checkbox -->
-                    <input type="checkbox" id="notificationCheckbox" style="transform: scale(1.5);">
-                    <!-- Menggunakan transform untuk memperbesar checkbox -->
-                    <label for="notificationCheckbox"></label>
-                    <br>
-                    <br>
-                    <br> <!-- Line break untuk memberikan jarak antara tombol dan checkbox -->
-                    <input type="checkbox" id="notificationCheckbox" style="transform: scale(1.5);">
-                    <!-- Menggunakan transform untuk memperbesar checkbox -->
-                    <label for="notificationCheckbox"></label>
-                </div>
-
-
-                <div style="float: left; width: 20%;color: white;">
-                    <h1>Email</h1>
-                    <br>
-                    <br>
-                    <br> <!-- Line break untuk memberikan jarak antara tombol dan checkbox -->
-                    <input type="checkbox" id="notificationCheckbox" style="transform: scale(1.5);">
-                    <!-- Menggunakan transform untuk memperbesar checkbox -->
-                    <label for="notificationCheckbox"></label>
-                    <br>
-                    <br>
-                    <br> <!-- Line break untuk memberikan jarak antara tombol dan checkbox -->
-                    <input type="checkbox" id="notificationCheckbox" style="transform: scale(1.5);">
-                    <!-- Menggunakan transform untuk memperbesar checkbox -->
-                    <label for="notificationCheckbox"></label>
-                </div>
+        <div class="row p-1">
+            <div class="col-md-6 p-1">
+                <h1 class="text-light">Pengaturan</h1>
+            </div>
+            <div class="col-md-6 p-1">
+                <button onclick="toggleMode()"class="btn btn-secondary">
+                    <h4>Toggle Mode</h4>
+                </button>
+            </div>
+        </div>
+        <div class="row justify-content-center align-items-center m-2">
+            <div class="col-md-6">
+                <h3>Notifikasi</h3>
+            </div>
+            <div class="col-md-3 text-center p-0">
+                <h4 class="text-light">Push Notification</h4>
+                <button id="aktivasiButton" style="background-color: red"
+                    onclick="showNotification()"class="btn text-light">AKTIVASI</button>
+            </div>
+            <div class="col-md-3 text-center">
+                <h1 class="text-light">Email</h1>
+            </div>
+        </div>
+        <div class="row justify-content-center align-items-center m-2">
+            <div class="col-md-6">
+                <h3>Pengumuman Baru</h3>
+            </div>
+            <div class="col-md-3 text-center p-0">
+                <input type="checkbox" id="notificationCheckbox" style="transform: scale(1.5);">
+            </div>
+            <div class="col-md-3 text-center">
+                <input type="checkbox" id="notificationCheckbox" style="transform: scale(1.5);">
+            </div>
+        </div>
+        <div class="row justify-content-center align-items-center m-2">
+            <div class="col-md-6">
+                <h3 style="color: white">Pengingat pembayaran</h3>
+            </div>
+            <div class="col-md-3 text-center p-0">
+                <input type="checkbox" id="notificationCheckbox" style="transform: scale(1.5);">
+            </div>
+            <div class="col-md-3 text-center">
+                <input type="checkbox" id="notificationCheckbox" style="transform: scale(1.5);">
+            </div>
         </div>
     </div>
 
@@ -77,7 +71,12 @@
                             body: 'Oke, Anda akan diberi pengingat.',
                         });
                     }
+                    // Tampilkan alert, baik izin notifikasi diberikan atau tidak
+                    alert('Oke, Anda akan diberi pengingat.');
                 });
+            } else {
+                // Jika browser tidak mendukung notifikasi, tampilkan alert
+                alert('Oke, Anda akan diberi pengingat.');
             }
         }
     </script>
